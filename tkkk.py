380f96272667481e426d470a3d4f02fcb76042d8
nisha710 <69087895+nisha710@users.noreply.github.com>
2022-12-23 16:56:13 +0530

    Add files via upload
    CODE.py

---
+++ tkkk.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Matplotlib Line :
+ #draws line in x -y plane
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+plt.plot(xpoints, ypoints)
+plt.show()
