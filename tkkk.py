8f0ac074698b1dfba1de1f2947c92977610d0b3a
nisha710 <69087895+nisha710@users.noreply.github.com>
2023-03-10 19:19:21 +0530

    Delete tkkk.py

--- tkkk.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-#Matplotlib Line :
- #draws line in x -y plane
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
-plt.plot(xpoints, ypoints)
-plt.show()
